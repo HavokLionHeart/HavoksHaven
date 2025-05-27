cf9d4f723770a4748891c387f7c5cc4fb14304bd
HavokLionHeart <Havoksmagicden@gmail.com>
2025-05-27 04:45:54 +0000

    maybe now?

--- classes/Resource.js
+++ classes/Resource.js
@@ -1,3 +1,29 @@
+class FloatingText {
+  constructor(x, y, text, color = "#FFF") {
+    this.x = x;
+    this.y = y;
+    this.text = text;
+    this.alpha = 1;
+    this.color = color;
+  }
+
+  update() {
+    this.y -= 0.5;
+    this.alpha -= 0.01;
+  }
+
+  draw(ctx) {
+    ctx.globalAlpha = this.alpha;
+    ctx.fillStyle = this.color;
+    ctx.font = "14px sans-serif";
+    ctx.fillText(this.text, this.x, this.y);
+    ctx.globalAlpha = 1;
+  }
+
+  isDead() {
+    return this.alpha <= 0;
+  }
+}
 export default class Resource {
     constructor(x, y, size, hp, type, color, rewards) {
       this.x = x;
